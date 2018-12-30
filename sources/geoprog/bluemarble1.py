1b3c8e98631f01333cc081672721276a2815cf8d
Jörg Kantel <joerg@kantel.de>
2018-12-30 13:28:49 +0100

    Größere Karte

--- sources/geoprog/bluemarble1.py
+++ sources/geoprog/bluemarble1.py
@@ -7,27 +7,23 @@ file_path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(file_path)
 
 wn = t.Screen()
-wn.setup(width = 800, height = 400)
+wn.setup(width = 1000, height = 500)
 wn.colormode(255)
-wn.bgpic("images/bluemarble.png")
+wn.bgpic("images/earthmap.png")
 wn.setworldcoordinates(-180, -90, 180, 90)
 wn.title("Blue Marble")
-
-# Testdaten
-# lat = 52.518611
-# lon = 13.408333
 
 city = t.Turtle()
 city.penup()
 city.color("red")
 city.shape("circle")
-city.shapesize(0.2)
+city.shapesize(0.4)
 wn.tracer(0)
 
 file = open("cities.csv", "r")
 cities = csv.reader(file, delimiter = ",")
 for c in cities:
-    print(c[1], c[2])
+    # print(c[1], c[2])
     lat = float(c[1])
     # lat = lat - math.cos(lat)
     lon = float(c[2])
