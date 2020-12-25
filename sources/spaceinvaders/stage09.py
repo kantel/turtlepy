01e62289b1d729bf9df40674b182d4278793488e
Jörg Kantel <joerg@kantel.de>
2020-12-25 11:02:25 +0100

    Stage 09 Update

--- sources/spaceinvaders/stage09.py
+++ sources/spaceinvaders/stage09.py
@@ -158,9 +158,9 @@ os.chdir(file_path)
 wn = t.Screen()
 wn.bgcolor("#000000")
 wn.bgcolor("#2b3e50")
-path_to_bg = os.path.join(os.getcwd(), "farback.gif")
+path_to_bg = "farback.gif"
 wn.bgpic(path_to_bg)
-ufo = os.path.join(os.getcwd(), "ufo.gif")
+ufo = "ufo.gif"
 # ufo = "ufo.gif"
 wn.register_shape(ufo)
 
