d2341bd91fa7f61ccc3893696e5d0849c98fe9cb
Jörg Kantel <joerg@kantel.de>
2023-07-29 18:42:04 +0200

    Turtle hochaufösend speichern

--- sources/pythagoras3.py
+++ sources/pythagoras3.py
@@ -1,18 +1,22 @@
+# Asymmetrischer Pythagoras-Baum
+# Wird als Postcript-File gespeichert und dann
+# mittels PIL nach .jpg/.png konvertiert
+
 import turtle as t
 import math
+from PIL import Image
 
-WIDTH = 640
-HEIGHT = 480
+WIDTH = 640  # 640
+HEIGHT = 480 # 480
 REC_LEVEL = 12      # Rekursions-Tiefe
 
-palette = [(189, 183, 110), (34, 139, 105), (154, 205, 50),
-           (139, 115, 85), (85, 107, 47), (139, 69, 19),
-           (107, 142, 35), (139, 134, 78), (152, 251, 152),
-           (0, 100, 0)]
+palette = [(42, 40, 45), (160, 51, 46), (54, 50, 80),
+           (50, 80, 105), (180, 144, 55),
+           (215, 158, 40), (140, 82, 48)]
 
 wn = t.Screen()
 wn.colormode(255)
-wn.bgcolor("#2b3e50")
+wn.bgcolor(230, 226, 204)
 wn.setup(width = WIDTH, height = HEIGHT)
 wn.title("Asymmetrischer Pythagoras-Baum")
 
@@ -45,7 +49,7 @@ def tree(s, level):
         p.back(s)
 
 def quadrat(s):
-    p.color(palette[int(s - 2)%10], palette[int(s - 2)%10])
+    p.color(palette[int(s)%len(palette)], palette[int(s)%len(palette)])
     p.begin_fill()
     for _ in range(4):
         p.forward(s)
@@ -60,8 +64,17 @@ p.pendown()
 # Für eine Rekursionstiefe > 14 braucht man schon sehr viel Geduld
 tree(85, REC_LEVEL) 
 p.hideturtle()
-print("I did it, Babe")
 # Bildschirm-Refresh wieder einschalten
 wn.update()
+
+file_name = "pythagoras"
+cv = wn.getcanvas()
+cv.postscript(file = file_name + ".eps", width = WIDTH, height = HEIGHT)
+# p.getscreen().getcanvas().postscript(file = file_name + ".eps", width = WIDTH, height = HEIGHT)
+
+img = Image.open(file_name + ".eps")
+img.save(file_name + ".png")
+
+print("I did it, Babe!")
 
 wn.mainloop()
