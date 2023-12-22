de0e5d2d9451177d41915643824163b24c7b0f9f
Jörg Kantel <1219909+kantel@users.noreply.github.com>
2023-12-22 15:37:50 +0100

    Update spiral.py

--- sources/spiral.py
+++ sources/spiral.py
@@ -3,7 +3,7 @@ import turtle as t
 WIDTH = 600
 HEIGHT = 600
 
-pal = [(1, 155, 183), (226, 107, 67), (240, 192, 68)]
+pal = [(1, 155, 183), (226, 107, 67), (240, 192, 68), (144, 166, 215)]
 
 wn = t.Screen()
 wn.colormode(255)
@@ -16,9 +16,16 @@ p.width(2)
 p.speed(0)
 p.hideturtle()
 
+# for i in range(300):
+#     p.color(pal[i%3])
+#     p.forward(i*1.5)
+#     p.right(121)
+
 for i in range(300):
-    p.color(pal[i%3])
-    p.forward(i*1.5)
-    p.right(121)
+    p.color(pal[i%4])
+    p.forward(i*1.2)
+    p.right(91)
+
+print("I did it, Babe!")
 
 wn.mainloop()
