92b09713bd71ca8b0e3aa1bbe5899251c59e4101
Jörg Kantel <1219909+kantel@users.noreply.github.com>
2024-01-04 20:25:25 +0100

    Update hexrandomwalk3.py

--- sources/hexrandomwalk3.py
+++ sources/hexrandomwalk3.py
@@ -7,8 +7,8 @@ MIN_X = -WIDTH//2 + 10
 MAX_X = WIDTH//2 - 20
 MIN_Y = HEIGHT//2 - 10
 MAX_Y = -HEIGHT//2 + 20 
-STEPSIZE = 20
 BORDERSIZE = 1
+STEPSIZE = 15
 
 codingtrain = ["#f05025", "#f89e50", "#f8ef22", "#f063a4",
                "#9252a1", "#817ac6", "#62c777", "#31c5f4"]
@@ -38,58 +38,34 @@ hexi = t.Turtle()
 hexi.speed(0)
 hexi.pensize(2)
 
+def go_home():
+    hexi.penup()
+    hexi.home()
+    hexi.pendown()
+
 for step in range(5000):
 
-    # Set Poncolor
+    # Set Pencolor
     if step%20 == 0:
         hexi.pencolor(codingtrain[r.randint(0, len(codingtrain) - 1)])
 
     # Roll Dice and set Angle
-    roll = r.randint(1, 6)
-    if roll == 1:
-        hexi.seth(0)
-    elif roll == 2:
-        hexi.seth(60)
-    elif roll == 3:
-        hexi.seth(120)
-    elif roll == 4:
-        hexi.seth(180)
-    elif roll == 5:
-        hexi.seth(240)
-    elif roll == 6:
-        hexi.seth(300)
-    else:
-        print("Es ist etwas faul im Staate Dänemark!")
+    roll = r.randint(0, 5)
+    angle = roll*60
+    hexi.seth(angle)
     
     # Check Border
-    if hexi.xcor() >= MAX_X - STEPSIZE:
-        # hexi.penup()
-        # hexi.setx(MIN_X + STEPSIZE)
-        # hexi.pendown()
-        STEPSIZE *= -1
-        hexi.fd(STEPSIZE)
-    elif hexi.xcor() <= MIN_X + STEPSIZE: 
-        # hexi.penup()
-        # hexi.setx(MAX_X - STEPSIZE)
-        # hexi.pendown()
-        STEPSIZE *= -1
-        hexi.fd(STEPSIZE)
-    elif hexi.ycor() <= MAX_Y + STEPSIZE:
-        # hexi.penup()
-        # hexi.sety(MIN_Y - STEPSIZE)
-        # hexi.pendown()
-        STEPSIZE *= -1
-        hexi.fd(STEPSIZE)
-    elif hexi.ycor() >= MIN_Y - STEPSIZE:
-        # hexi.penup()
-        # hexi.sety(MAX_Y + STEPSIZE)
-        # hexi.pendown()
-        STEPSIZE *= -1
-        hexi.fd(STEPSIZE)
+    if hexi.xcor() >= MAX_X:
+        go_home()
+    elif hexi.xcor() <= MIN_X:
+        go_home()
+    elif hexi.ycor() <= MAX_Y:
+        go_home()
+    elif hexi.ycor() >= MIN_Y:
+        go_home()
     else:
         hexi.fd(STEPSIZE)
 
 print("I did it, Babe!")
 wn.exitonclick()
-
 wn.mainloop()
